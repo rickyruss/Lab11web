4c83cad07a627c9fe23ba4d83b6b4df5b1f5c6e5
rickyruss <56240498+rickyruss@users.noreply.github.com>
2021-06-15 23:56:21 +0700

    Add files via upload

---
+++ Views/about.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+<head> 
+    <meta charset="UTF-8"> 
+    <title><?= $title; ?></title> 
+</head> 
+<body>
+    <?= $this->include('template/header'); ?>
+    <section id="introduce">
+        <div class="row">
+            <img src="ayam.jpg" title="Ricky Rusandi" alt="Rickyyy" class="image-circle" width="230"
+            style="float: left; border: 2px solid black;">
+            <h1>HAIII!</h1>
+            <p>Nama saya Ricky Rusandi. Saya adalah seorang mahasiswa dari <i>Universitas Pelita Bangsa</i> yang saat ini sedang
+                mempelajari materi PHP Framework dengan menggunakan (Codeigniter) dalam mata kuliah <i>Pemrograman Web</i>.</p>
+        </div>
+    </section>
+    <?= $this->include('template/footer'); ?>
+</body> 
+</html>
